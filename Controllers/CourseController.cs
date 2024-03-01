f8b5b00d2c21b60ee9ad62922f96e8a005c7fc09
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-03-01 06:05:44 +0300

    updated

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BtkAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+    }
+}
