4794b90287f057a19bb92333e521ec0c05472351
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-03-03 03:19:31 +0300

    update StoreApp

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,7 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
 
 app.Run();
 
