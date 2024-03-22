ae72ccad734ed5eb0951c485281c13b245dc7a35
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-03-23 02:03:57 +0300

    Manage Orders created

---
+++ Store/StoreApp/Areas/Admin/Controllers/OrderrController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderrController : Controller
+    {
+        private readonly IServiceManager _manager;
+
+        public OrderrController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public IActionResult Index()
+        {
+            var order = _manager.OrderService.Orders;
+            return View(order);
+        }
+
+        [HttpPost]
+        public IActionResult Complete([FromForm] int id)
+        {
+            _manager.OrderService.Complete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
