1a2b3caa263a75f9df4ff95824d8f6316b210acd
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-03-24 01:58:29 +0300

    Extensions Routing Created

--- Store/StoreApp/Infrastructe/Extensions/ServiceExtension.cs
+++ Store/StoreApp/Infrastructe/Extensions/ServiceExtension.cs
@@ -46,5 +46,14 @@ namespace StoreApp.Infrastructe.Extensions
             services.AddScoped<ICategoryService, CategoryManager>();
             services.AddScoped<IOrderService, OrderManager>();
         }
+
+        public static void ConfigureRouting(this IServiceCollection services)
+        {
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = false;
+            });
+        }
     }
 }
