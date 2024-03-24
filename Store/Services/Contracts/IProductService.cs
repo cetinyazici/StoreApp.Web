f178cea5730ff07823a825601e30ffa850867c97
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-03-25 02:07:43 +0300

    TagBuilder created

--- Store/Services/Contracts/IProductService.cs
+++ Store/Services/Contracts/IProductService.cs
@@ -12,6 +12,7 @@ namespace Services.Contracts
     public interface IProductService
     {
         IEnumerable<Product> GetAllProducts(bool trackChanges);
+        IEnumerable<Product> GetLastestProducts(int n, bool trackChanges);
         IEnumerable<Product> GetAllProductsWithDetails(ProductRequestParameters p);
         Product? GetOneProduct(int id, bool trackChanges);
         IEnumerable<Product> GetShowcaseProducts(bool trackChanges);    
---
+++ Store/StoreApp/Infrastructe/TagHelpers/LastestProductTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Services.Contracts;
+
+namespace StoreApp.Infrastructe.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("div", Attributes = "products")]
+    public class LastestProductTagHelper : TagHelper
+    {
+        private readonly IServiceManager _manager;
+
+        public LastestProductTagHelper(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagBuilder div = new TagBuilder("div");
+            div.Attributes.Add("class", "my-3");
+
+            TagBuilder h6 = new TagBuilder("h6");
+            h6.Attributes.Add("class", "lead");
+
+            TagBuilder icon = new TagBuilder("i");
+            icon.Attributes.Add("class", "fa fa-box text-secondary");
+
+            h6.InnerHtml.AppendHtml(icon);
+            h6.InnerHtml.AppendHtml("Lastest Products");
+
+            TagBuilder ul = new TagBuilder("ul");
+            var products = _manager.ProductService.GetLastestProducts(5,false);
+            foreach (Product prd in products)
+            {
+                TagBuilder li = new TagBuilder("li");
+                
+                TagBuilder a = new TagBuilder("a");
+                a.Attributes.Add("href",$"/product/get/{prd.ProductId}");
+                a.InnerHtml.AppendHtml(prd.ProductName);
+                
+                li.InnerHtml.AppendHtml(a);
+                ul.InnerHtml.AppendHtml(li); 
+            }
+
+            div.InnerHtml.AppendHtml(h6);
+            div.InnerHtml.AppendHtml(ul);
+            output.Content.AppendHtml(div);
+        }
+    }
+}
