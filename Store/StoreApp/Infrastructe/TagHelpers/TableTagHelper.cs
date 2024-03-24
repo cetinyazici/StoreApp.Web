9610249927d7f501a3b40bf9221ccca33d540bb1
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-03-24 19:26:11 +0300

    TagHelpers created

---
+++ Store/StoreApp/Infrastructe/TagHelpers/TableTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StoreApp.Infrastructe.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("table")]
+    public class TableTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", "table table-hover");
+        }
+    }
+}
